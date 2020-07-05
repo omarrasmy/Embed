049827a25e78f6da842029cfd954e4c2f65176e2
omarrasmy <omarrasmy00@gmail.com>
2020-07-05 17:12:34 +0200

    sss

--- omar.py
+++ omar.py
@@ -42,5 +42,5 @@ class GenerateEmbed(Resource):
 
 api.add_resource(GenerateEmbed, '/GenerateEmbed')  # Route_1
 if __name__ == '__main__':
-    port = int(os.environ.get("VCAP_APP_PORT", 5000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
